5c68cbedcaa619e812749603f9648d1d05eb0392
Jerocardona27 <jeronimocardona70@gmail.com>
2024-01-30 14:51:38 -0500

    mawuel

--- src/assets/helpers/contains.js
+++ src/assets/helpers/contains.js
@@ -1,2 +1,2 @@
-export const url = "https://api-adopcion-glrb.vercel.app/img-perritos"
-export const url2 = "https://api-adopcion.vercel.app/img-gaticos"
+export const url = "https://apiadopcionnew.onrender.com/img-perritos"
+export const url2 = "https://apiadopcionnew.onrender.com/img-gaticos"
